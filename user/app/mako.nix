ebdecd771d13a21cb79227e1691aa7fe724b20aa
ilia-luk <ilia@domusnetwork.io>
2025-06-17 15:55:08 +0700

    fixes mako notifications

--- user/app/mako.nix
+++ user/app/mako.nix
@@ -5,26 +5,30 @@ with config.lib.stylix.colors; {
   services.mako.enable = true;
   services.mako = {
     settings = {
-      font = userSettings.font + " 16";
-      margin = "12";
-      padding = "12";
-      textColor = "#${base05}ff";
-      borderSize = 2;
-      borderColor = "#${base0E}ff";
-      borderRadius = 8;
-      progressColor = "#${base0E}ff";
-      backgroundColor = "#${base00}bf";
+      actions = 1;
+      history = 1;
+      width = 480;
+      height = 192;
+      anchor = "top-right";
+      font = userSettings.font + " 14";
+      margin = "29,32";
+      padding = "16,24";
+      text-color = "#${base05}ff";
+      border-size = 2;
+      border-color = "#${base0E}ff";
+      border-radius = 8;
+      progress-color = "source #${base0E}ff";
+      background-color = "#${base00}bf";
       icons = true;
+      max-icon-size = 64;
+      ignore-timeout = false;
+      layer = "top";
+      markup = true;
       sort = "-time";
-      defaultTimeout = 3000;
-      extraConfig = ''
-        [urgency=low]
-        border-color=#${base0B}
-        [urgency=normal]
-        border-color=#${base0E}
-        [urgency=high]
-        border-color=#${base08}
-      '';
+      default-timeout = 12000;
+      "urgency=low" = { border-color = "#${base0B}"; };
+      "urgency=normal" = { border-color = "#${base0E}"; };
+      "urgency=high" = { border-color = "#${base08}"; };
     };
   };
 }
