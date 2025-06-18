e380efda20345b09a198f5e1403d63851305a523
ilia-luk <ilia@domusnetwork.io>
2025-06-18 18:10:56 +0700

    adds postgres pgadmin dbeaver

--- system/db/postgresql.nix
+++ system/db/postgresql.nix
@@ -1,23 +1,47 @@
-{ pkgs, userSettings, ... }: {
-  environment.systemPackages = with pkgs; [ postgresql pgadmin ];
+{ pkgs, lib, userSettings, ... }: {
+  environment.systemPackages = with pkgs; [
+    unstable.postgresql
+    unstable.pgadmin
+    unstable.dbeaver-bin
+  ];
 
   services.postgresql = {
     enable = true;
-    settings = { port = 15432; };
+    settings = {
+      port = 5432;
+      log_connections = true;
+      log_statement = "all";
+      logging_collector = true;
+      log_disconnections = true;
+      log_destination = lib.mkForce "syslog";
+    };
     ensureUsers = [{
-      name = "lamp";
-      # ensurePermissions = {
-      #   "DATABASE lamp" = "ALL PRIVILEGES";
-      # };
+      name = "admin";
+      ensureDBOwnership = true;
+      ensureClauses = {
+        superuser = true;
+        createrole = true;
+        createdb = true;
+      };
     }];
-    ensureDatabases = [ "lamp" ];
+    ensureDatabases = [ "mydatabase" "admin" ];
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #...
+      #type database DBuser origin-address auth-method
+      local all       all     trust
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+    '';
   };
 
-  # services.pgadmin = {
-  #   enable = true;
-  #   initialEmail = userSettings.email; 
-  #   initialPasswordFile = pkgs.writeText "pgadminPW" ''
-  #     admin
-  #   '';
-  # };
+  services.pgadmin = {
+    enable = true;
+    initialEmail = userSettings.email;
+    initialPasswordFile = pkgs.writeText "pgadminPW" ''
+      rootadmin
+    '';
+  };
 }
