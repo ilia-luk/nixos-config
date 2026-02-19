cf79a4d7251b9275597e42e30ead2d6a58e81622
ilia-luk <ilia@domusnetwork.io>
2026-02-19 14:26:30 +0700

    WIP: try fixing crush after suspend

--- system/hardware/systemd.nix
+++ system/hardware/systemd.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  services.journald.extraConfig = "SystemMaxUse=50M\nSystemMaxFiles=5";
+  services.journald.extraConfig = ''
+    SystemMaxUse=50M
+    SystemMaxFiles=5'';
   services.journald.rateLimitBurst = 500;
   services.journald.rateLimitInterval = "30s";
+
+  systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS =
+    "false";
+  systemd.services."pre-sleep".wantedBy = lib.mkForce [ ];
 }
