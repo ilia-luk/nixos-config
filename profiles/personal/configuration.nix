31b41151edcb56edc1e8454bee0576fde2abb0fe
ilia-luk <ilia@domusnetwork.io>
2026-07-24 17:13:46 +0700

    limit boot generations

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -123,6 +123,9 @@
   # Desktop portals
   xdg.portal.enable = true;
 
+  # Limit boot to 5 generations of kernels
+  boot.loader.systemd-boot.configurationLimit = 5;
+
   # Leave this unchanged for compatibility purposes
   system.stateVersion = "24.05";
 
