d09dc904e9a6718efd0aba2305c4d31f1a62679e
ilia-luk <ilia@domusnetwork.io>
2026-07-27 19:42:15 +0700

    WIP: fix librewolf 1

--- user/app/librewolf.nix
+++ user/app/librewolf.nix
@@ -4,15 +4,19 @@
     profiles = {
       ilia = {
         isDefault = true;
+        settings."gfx.webrender.software" = true;
       };
       xpression = {
         id = 1;
+        settings."gfx.webrender.software" = true;
       };
       accounts-domus = {
         id = 2;
+        settings."gfx.webrender.software" = true;
         # settings."privacy.clearOnShutdown.cookies" = false;  # uncomment if MFA-every-restart gets old
       };
       domusnetwork = {
+        settings."gfx.webrender.software" = true;
         id = 3;
       };
     };
