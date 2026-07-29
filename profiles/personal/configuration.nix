319be006eefb51b5de48c735b5218dc9a3fd46ab
ilia-luk <ilia@domusnetwork.io>
2026-07-30 01:37:40 +0700

    adds devenv template 6

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -157,11 +157,13 @@
       "https://noctalia.cachix.org"
       "https://pi.cachix.org"
       "https://nix-community.cachix.org"
+      "https://devenv.cachix.org"
     ];
     extra-trusted-public-keys = [
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "pi.cachix.org-1:lGeoGJaZ5ZDabuRzkcD5EBTNnDM4HJ1vqeOxlWk1Flk="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 }
