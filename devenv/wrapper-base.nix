200c5c3dc4369f6bff64576720b5411138fc0a01
ilia-luk <ilia@domusnetwork.io>
2026-07-30 19:07:10 +0700

    wip: adding worktrunk 2

--- devenv/wrapper-base.nix
+++ devenv/wrapper-base.nix
@@ -69,7 +69,7 @@ in
       env-sync.exec = ''
         set -e
         echo ">> syncing wrapper against dotfiles..."
-        nix flake update dotfiles
+        nix flake update dotfiles nixpkgs-unstable
         git add flake.lock
         echo ">> lock updated; direnv reloads on next prompt (or: direnv reload)"
       '';
