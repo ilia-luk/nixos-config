89a57a88f077647bbbee231538e0086cc9372c54
ilia-luk <ilia@domusnetwork.io>
2026-09-08 22:04:10 +0700

    removes direnv global load of .env

--- user/shell/direnv.nix
+++ user/shell/direnv.nix
@@ -1,18 +1,19 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs; [ direnv nix-direnv ];
+  home.packages = with pkgs; [
+    direnv
+    nix-direnv
+  ];
 
   programs.direnv = {
     enable = true;
     enableNushellIntegration = true;
     nix-direnv.enable = true;
     config = {
-      global = { load_dotenv = true; };
       whitelist = {
         prefix = [
-          "${config.home.homeDirectory}/dev/ehouse/"
           "${config.home.homeDirectory}/dev/domusnetwork/"
           "${config.home.homeDirectory}/dev/accountant/"
-          "${config.home.homeDirectory}/dev/riverpool/"
+          "${config.home.homeDirectory}/dev/automation/"
         ];
       };
     };
